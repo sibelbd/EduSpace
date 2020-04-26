ed5bef7cacf5559ac931ff9ded0b9fd3134e34c4
Shreyan Mohanty <shreyanmohanty4@gmail.com>
2020-04-26 19:52:24 +0530

    build app

---
+++ Frontend/build/precache-manifest.3dc078aa102994128bf9ec033801ad9c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbe3894af7f20714319149a1751f926a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d313cc937d24631822b6",
+    "url": "/static/css/2.4f9c9927.chunk.css"
+  },
+  {
+    "revision": "cdbfd7988733af2ef0e8",
+    "url": "/static/css/main.3bdb04bd.chunk.css"
+  },
+  {
+    "revision": "d313cc937d24631822b6",
+    "url": "/static/js/2.cb60dbae.chunk.js"
+  },
+  {
+    "revision": "02435e68846c6502db7cf947492b1248",
+    "url": "/static/js/2.cb60dbae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cdbfd7988733af2ef0e8",
+    "url": "/static/js/main.f119856b.chunk.js"
+  },
+  {
+    "revision": "5bbf25f4fb6d1dc9ef02",
+    "url": "/static/js/runtime-main.1596afcd.js"
+  },
+  {
+    "revision": "4aaa1bc235000f232ede331fb7bb1d6b",
+    "url": "/static/media/NotFound.4aaa1bc2.jpg"
+  },
+  {
+    "revision": "3711b1b12874f358d0ed692f40d1082e",
+    "url": "/static/media/dp.3711b1b1.jpg"
+  }
+]);
